675ac46954b62c49244194616a319a73cb435974
Ian <iansaccar7@gmail.com>
2025-06-08 15:44:35 -0300

    feat: add basename to BrowserRouter for routing configuration

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'aos/dist/aos.css'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Importando o BrowserRouter
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'aos/dist/aos.css';
+import './index.css';
 
+// Adicionando o basename para o BrowserRouter
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/brandalizesm">  {/* Definindo o basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
